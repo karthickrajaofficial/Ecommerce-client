9f6f18e91d0ad7f05660b3046ffa348ff04d9530
karthickrajaofficial <karthickrajaofficial1@gmail.com>
2024-06-20 10:44:36 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -43,5 +43,8 @@ app.use('/api/payment', paymentRoutes);
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname+ "/uploads")));
-
+app.use(cors({
+  credentials: true,
+  origin: true,
+}));
 app.listen(port , ()=> console.log(`server running on port : ${port}`))
